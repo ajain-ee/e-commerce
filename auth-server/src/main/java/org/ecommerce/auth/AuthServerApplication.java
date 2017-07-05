cec013cb16d862b4c0b8b0b4767e4afe7b9fb2df
anuj-jain <anuj.jain@digital.hmrc.gov.uk>
2017-07-05 12:48:23 +0530

    Added Zuul proxy to routes access to auth server and product service

--- auth-server/src/main/java/org/ecommerce/auth/AuthServerApplication.java
+++ auth-server/src/main/java/org/ecommerce/auth/AuthServerApplication.java
@@ -3,10 +3,22 @@ package org.ecommerce.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
+@EnableResourceServer
 public class AuthServerApplication {
+
+    @RequestMapping(path = "/users")
+    public Principal user(Principal user){
+        return user;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServerApplication.class,args);
